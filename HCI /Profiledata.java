9b4d71c3cce0d10aa7759d4555ffd4e26c9b2d29
lee <shanleebaldonado24@gmail.com>
2026-08-09 22:31:48 +0800

    Profiledata.java with personal information

---
+++ HCI /Profiledata.java
@@ -0,0 +1,14 @@
+public class Main 
+{
+    public static void main(String[] args) {
+
+        System.out.println("🐶 🐶 🐶");
+
+        System.out.println("Name       : Shanlee Baldonado (Shan)");
+        System.out.println("Birthday   : 07/05/2006");
+        System.out.println("Address    : Sta. Monica, Oton");
+        System.out.println("Fav Song   : BIRDS OF A FEATHER by:Billie Eilish");
+        System.out.println("Motivation : My Family and Friends");
+        System.out.println("Support    : God,Family and Friends");
+    }
+}
