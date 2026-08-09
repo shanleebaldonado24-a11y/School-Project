f1409a6b4bc2b0ce25a8ca6ed81eda25c79ce2c2
lee <shanleebaldonado24@gmail.com>
2026-08-09 22:36:39 +0800

    Add files via upload

---
+++ OPP/Largest Finders.java
@@ -0,0 +1,22 @@
+public class Largest Finders 
+{ 
+    public static int main (String [] args ) {
+
+        int a =45, b = 78, c = 23;
+        int largest;
+        
+    if  ("a>b && a > = c") 
+    {
+        largest = a;
+    }
+    else if ("b > a && b > = c")
+    {
+        largest = b;
+    }
+    else 
+    {
+        largest = c;
+    }
+    System.out.println("The largest number is: " + largest);
+}
+    }
---
+++ OPP/public class TotalCalculator.java
@@ -0,0 +1,16 @@
+public class TotalCalculator
+{
+
+    public static void main(String[] args) {
+
+        // Declare variables
+        int x = 80;
+        int y = 21;
+
+        // Compute total
+        int total = x + y;
+
+        // Print result
+        System.out.println("Total: " + total);
+    }
+}
